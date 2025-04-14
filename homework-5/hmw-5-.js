12b773746ba34c0596bbd538b7684f7bc8fa5402
shuhrat6878 <abbosovich1999@gmail.com>
2025-04-14 08:15:13 +0500

    dawomi

---
+++ homework-5/hmw-5-.js
@@ -0,0 +1,92 @@
+// 1-misol
+
+// let a = +prompt('1 da 100 gacha baho kiriting !!');
+
+// if (90 < a && a < 100) {
+//     console.log('Alo');
+// }else if (70 < a && a < 89){
+//     console.log("Yaxshi");
+// }else if (60 < a && a < 69){
+//     console.log("Qoniqarli");
+// }else if (0 < a && a < 59){
+//     console.log("Qoniqarsiz");
+// }
+
+// 2-miaol
+
+// let a = +prompt('1 da 7 gacha Raqam kiriting !!');
+
+// switch (a) {
+//     case 1:
+//         console.log('Dushanba');
+//         break;
+//     case 2:
+//         console.log('Seshanba');
+//         break;
+//     case 3:
+//         console.log('Chorshanba');
+//         break;
+//     case 4:
+//         console.log('Payshanba');
+//         break;
+//     case 5:
+//         console.log('Juma');
+//         break;
+//     case 6:
+//         console.log('Shanba');
+//         break;
+//     case 7:
+//         console.log('Yakshanba');
+//         break;
+//     default:{
+//         console.log("Bunday hafta kuni yoq")
+//     }
+// }
+
+// 3-misoll
+
+// let a = +prompt('yosh ucun Raqam kiriting !!');
+
+// if (0 < a && a <= 12) {
+//     console.log("Bola");
+// } else if (13 <= a && a <= 19) {
+//     console.log("'O'smir");
+// }else if (20 <= a && a <= 59) {
+//     console.log("Katta");
+// }else {
+//     console.log("Qariya");
+// }
+//  4-misol
+// function engKattasi(a, b, c) {
+//   let katta;
+
+//   if (a >= b && a >= c) {
+//     katta = a;
+//   } else if (b >= a && b >= c) {
+//     katta = b;
+//   } else {
+//     katta = c;
+//   }
+
+//   return katta;
+// }
+
+// console.log(engKattasi(5, 8, 3));
+// console.log(engKattasi(10, 10, 10));
+// console.log(engKattasi(2, 2, 3));
+
+// 5-misoll
+
+// function logParol(login, parol) {
+//   if (login === "admin" && parol === "12345") {
+//     return "Xush kelibsiz, Admin";
+//   } else if (login === "user" && parol === "password") {
+//     return "Xush kelibsiz, Foydalanuvchi";
+//   } else {
+//     return "Login yoki parol xato";
+//   }
+// }
+
+// console.log(logParol("admin", "12345"));      
+// console.log(logParol("user", "password"));    
+// console.log(logParol("admin", "wrong"));      
